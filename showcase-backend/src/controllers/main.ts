2b2b0e5aa787feca7cbda747c0e7ba1c24c74cd6
Fidym <marci@fidy.hu>
2021-06-04 15:17:12 +0200

    chore: remove withdraw controller

--- showcase-backend/src/controllers/main.ts
+++ showcase-backend/src/controllers/main.ts
@@ -1,14 +1,10 @@
 import { WalletController } from './walletController'
-// import { NotificationController } from './notificationController'
 import { ChatController } from './chatController'
-import { WithdrawalsController } from './withdrawalsController'
 import { Application } from 'express'
 
 export const MainController = (app: Application) => {
   app.use('/wallet', WalletController)
-  // app.use('/notification', NotificationController)
   app.use('/chat', ChatController)
-  app.use('/withdrawals', WithdrawalsController)
   app.get('/__healthy', (req, res) => {
     res.send({ status: 'ok' })
   })

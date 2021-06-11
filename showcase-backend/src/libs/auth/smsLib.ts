976bc36f29419ac188843fff5cc0d09a75a8fa10
Fidym <marci@fidy.hu>
2021-06-11 14:44:08 +0200

    feat: twilio refactor

--- showcase-backend/src/libs/auth/smsLib.ts
+++ showcase-backend/src/libs/auth/smsLib.ts
@@ -1,7 +1,6 @@
 import validator from 'validator'
 import { SendPhoneCodeInput, SendPhoneCodeResponse } from './types/sendPhoneCode.type'
-import { twilio as twilioConfig } from '../../config'
-import { twilio } from '../../services/twilio'
+import { myTwilio } from '../../services/twilio'
 import moment from 'moment'
 import {
   AUTH_CODE_EXPIRATION,
@@ -9,15 +8,17 @@ import {
   AUTH_MAX_ENTER_ATTEMP,
 } from '../../consts/businessRules'
 import { AuthLib } from './authLib'
-import { prisma } from '../../services/prisma'
 import { GraphQLError } from 'graphql'
 import { VerifyPhoneCodeInput, VerifyPhoneCodeResponse } from './types/verifyPhoneCode.type'
 
-const INCREMENT_WITH_ONE = {
-  increment: 1,
-}
-
-interface TwilioSmsInput {
+import { SmsVerificationCreateInput } from '@generated/type-graphql'
+import {
+  findUniqueVerification,
+  incrementVerificationCount,
+  resetVerification,
+  upsertVerification,
+} from '../database/smsVerification.repo'
+interface SmsInput {
   phone: string
   code: string
 }
@@ -35,49 +36,30 @@ class SmsLib {
 
   verifyPhoneCode = async ({
     code,
-    phone: phoneNums,
+    phone,
     areaCode,
   }: VerifyPhoneCodeInput): Promise<VerifyPhoneCodeResponse> => {
-    const phone = `+${areaCode + phoneNums}`
-    await this.validateCode({ phone, code })
-    const { authId, isNewUser } = await AuthLib.findOrCreateNewUser({ phone, areaCode })
+    const validPhone = await this.validatePhoneNumber({ areaCode, phone })
+    await this.validateCode({ phone: validPhone, code })
+    const { authId, isNewUser } = await AuthLib.findOrCreateNewUser({ phone: validPhone, areaCode })
     const token = await AuthLib.getCustomToken(authId)
     return { token, isNewUser }
   }
 
-  validateCode = async ({ phone, code }: { phone: string; code: string }) => {
-    const verification = await prisma.smsVerification.findUnique({
-      where: {
-        phone,
-      },
-    })
-
-    if (!verification) {
-      throw new GraphQLError('No verification code sent to that number!')
-    }
-
-    const timeNow = new Date()
-
-    let error = null
-    if (verification.code !== code) {
-      error = 'custom/code-does-not-match'
-    } else if (!verification.valid) {
-      error = 'custom/code-already-used'
-    } else if (timeNow > verification.expiration) {
-      error = 'custom/code-expired'
-    }
-    if (error) {
-      await this.incrementVerificationCount(phone)
-      throw error
-    } else {
-      await this.resetVerification(phone)
+  validateCode = async ({ phone, code }: SmsInput) => {
+    try {
+      await this.customVerificationValidation(phone)
+      await myTwilio.checkVerificationToken(phone, code)
+      await resetVerification(phone)
+    } catch (error) {
+      await incrementVerificationCount(phone)
+      throw new GraphQLError(error)
     }
   }
 
   sendSmsAndUpdateVerification = async (phone: string) => {
-    const verificationData = {
+    const verificationData: SmsVerificationCreateInput = {
       phone,
-      code: this.generateSmsCode(),
       expiration: this.generateExpirationDate(),
       valid: true,
       codesSent: 1,
@@ -86,36 +68,32 @@ class SmsLib {
       attemptsEntered: 0,
     }
 
-    await this.validateSmsVerification(phone)
-    this.sendTwilioSmsWithCode({ phone, code: verificationData.code })
+    await this.checkIfCodeSentLimitExceeded(phone)
+    await myTwilio.sendVerificationToken(phone)
 
-    return await prisma.smsVerification.upsert({
-      where: {
-        phone,
-      },
-      update: {
-        ...verificationData,
-        codesSent: INCREMENT_WITH_ONE,
-        codesSentSinceValid: INCREMENT_WITH_ONE,
-      },
-      create: {
-        ...verificationData,
-      },
-    })
+    return await upsertVerification(verificationData)
   }
 
-  validateSmsVerification = async (phone: string) => {
-    const existingVerification = await prisma.smsVerification.findUnique({
-      where: {
-        phone,
-      },
-    })
+  checkIfCodeSentLimitExceeded = async (phone: string) => {
+    const existingVerification = await findUniqueVerification(phone)
 
     if (!existingVerification) return
 
     const { codesSent, attemptsEnteredSinceValid } = existingVerification
     if (codesSent > AUTH_MAX_CODE_SEND || attemptsEnteredSinceValid > AUTH_MAX_ENTER_ATTEMP) {
       throw new GraphQLError('Too many attempts with this number')
+    }
+  }
+
+  customVerificationValidation = async (phone: string) => {
+    const verification = await findUniqueVerification(phone)
+
+    if (!verification) {
+      throw new GraphQLError('No verification code sent to that number!')
+    } else if (!verification.valid) {
+      throw new GraphQLError('Code already used!')
+    } else if (new Date() > verification.expiration) {
+      throw new GraphQLError('Code expired!')
     }
   }
 
@@ -130,47 +108,8 @@ class SmsLib {
     })
   }
 
-  sendTwilioSmsWithCode = async ({ phone, code }: TwilioSmsInput) => {
-    await twilio.messages.create({
-      body: `Your Showcase login code is ${code}`,
-      to: `${phone}`,
-      from: twilioConfig.from,
-    })
-  }
-
-  generateSmsCode = (): string => {
-    return Math.floor(Math.random() * 899999 + 100000).toString()
-  }
-
   generateExpirationDate = (): Date => {
     return moment().add(AUTH_CODE_EXPIRATION, 'seconds').toDate()
-  }
-
-  incrementVerificationCount = async (phone: string) => {
-    return await prisma.smsVerification.update({
-      where: {
-        phone,
-      },
-      data: {
-        codesSent: INCREMENT_WITH_ONE,
-        codesSentSinceValid: INCREMENT_WITH_ONE,
-        attemptsEntered: INCREMENT_WITH_ONE,
-        attemptsEnteredSinceValid: INCREMENT_WITH_ONE,
-      },
-    })
-  }
-
-  resetVerification = async (phone: string) => {
-    return await prisma.smsVerification.update({
-      where: {
-        phone,
-      },
-      data: {
-        valid: false,
-        codesSentSinceValid: INCREMENT_WITH_ONE,
-        attemptsEnteredSinceValid: INCREMENT_WITH_ONE,
-      },
-    })
   }
 }
 
--- showcase-backend/src/services/twilio.ts
+++ showcase-backend/src/services/twilio.ts
@@ -1,6 +1,47 @@
 import { twilio as twilioConfig } from '../config'
-import twilio from 'twilio'
+import twilio, { Twilio } from 'twilio'
 
-const twiloClient = twilio(twilioConfig.account, twilioConfig.token)
+const client = twilio(twilioConfig.account, twilioConfig.token)
 
-export { twiloClient as twilio }
+class MyTwilio {
+  sid: string
+  client: Twilio
+
+  constructor() {
+    this.client = client
+  }
+
+  private ensureValidSid = async () => {
+    if (!this.sid) {
+      const service = await this.createVerificationService()
+      this.sid = service.sid
+    }
+  }
+
+  private createVerificationService = async () => {
+    return await this.client.verify.services.create({
+      friendlyName: 'Showcase',
+      codeLength: 6,
+      lookupEnabled: true,
+      skipSmsToLandlines: true,
+    })
+  }
+
+  sendVerificationToken = async (to: string) => {
+    await this.ensureValidSid()
+    return await this.client.verify
+      .services(this.sid)
+      .verifications.create({ to, channel: 'sms' })
+  }
+
+  checkVerificationToken = async (to: string, code: string) => {
+    await this.ensureValidSid()
+    return await this.client.verify
+      .services(this.sid)
+      .verificationChecks.create({ to, code })
+  }
+}
+
+const myTwilio = new MyTwilio()
+
+export { myTwilio }

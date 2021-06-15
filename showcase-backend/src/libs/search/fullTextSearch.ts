16e2e0c3be0fc4fd181aee711880254856189e99
Fidym <marci@fidy.hu>
2021-06-15 14:44:37 +0200

    fix: missing mode + some refactor

--- showcase-backend/src/libs/search/fullTextSearch.ts
+++ showcase-backend/src/libs/search/fullTextSearch.ts
@@ -1,92 +1,120 @@
 import prisma from '../../services/prisma'
 
+const mode = 'insensitive'
+const createdAt = 'desc'
+
+// todo: add pagination once concept is finalized
 export const fullTextSearch = async (search: string) => {
-    const mode =  "insensitive"
-    const [badgeTypes = [], profiles = [], causes =[]] = await prisma.$transaction([
-      prisma.badgeType.findMany({
-        where: {
-          OR: [
-            {
-              title: {
-                contains: search,
-                mode
-              },
-            },
-            {
-              description: {
-                contains: search,
-                mode
-              },
-            },
-            {
-              creator: {
-                profile: {
-                  OR: [
-                    {
-                      displayName: {
-                        contains: search,
-                        mode
-                      },
-                    },
-                    {
-                      username: {
-                        contains: search,
-                        mode
-                      },
-                    },
-                    {
-                      bio: {
-                        contains: search,
-                        mode
-                      },
-                    },
-                  ],
-                },
-              },
-            },
-          ],
-        },
-      }),
-      prisma.profile.findMany({
-        where: {
-          OR: [
-            {
-              displayName: {
-                contains: search,
-                mode
-              },
-            },
-            {
-              username: {
-                contains: search,
-                mode
-              },
-            },
-            {
-              bio: {
-                contains: search,
-                mode
-              },
-            },
-          ],
-        },
-      }),
-      prisma.cause.findMany({
-        where: {
-          OR: [
-            {
-              name: {
-                contains: search,
-              },
-            },
-            {
-              site: {
-                contains: search,
-              },
-            },
-          ],
-        },
-      }),
-    ])
+  const contains = search
+
+  const [badgeTypes = [], profiles = [], causes = []] = await prisma.$transaction([
+    searchBadgeTypes(contains),
+    searchProfiles(contains),
+    searchCauses(contains)
+  ])
   return { badgeTypes, profiles, causes }
+}
+
+const searchBadgeTypes = (contains: string) => {
+  return prisma.badgeType.findMany({
+    where: {
+      OR: [
+        {
+          title: {
+            contains,
+            mode,
+          },
+        },
+        {
+          description: {
+            contains,
+            mode,
+          },
+        },
+        {
+          creator: {
+            profile: {
+              OR: [
+                {
+                  displayName: {
+                    contains,
+                    mode,
+                  },
+                },
+                {
+                  username: {
+                    contains,
+                    mode,
+                  },
+                },
+                {
+                  bio: {
+                    contains,
+                    mode,
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    },
+    orderBy: {
+      createdAt,
+    }
+  })
+}
+
+const searchProfiles = (contains: string) => {
+  return prisma.profile.findMany({
+    where: {
+      OR: [
+        {
+          displayName: {
+            contains,
+            mode,
+          },
+        },
+        {
+          username: {
+            contains,
+            mode,
+          },
+        },
+        {
+          bio: {
+            contains,
+            mode,
+          },
+        },
+      ],
+    },
+    orderBy: {
+      createdAt,
+    },
+  })
+}
+
+const searchCauses = (contains: string) => {
+  return prisma.cause.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains,
+            mode,
+          },
+        },
+        {
+          site: {
+            contains,
+            mode,
+          },
+        },
+      ],
+    },
+    orderBy: {
+      createdAt,
+    },
+  })
 }

d985f866c38788df21f08bf1b388ebd9153508e5
Fidym <marci@fidy.hu>
2021-06-16 16:14:27 +0200

    chore: remove unused vars

--- showcase-backend/src/resolvers/customUserResolver.ts
+++ showcase-backend/src/resolvers/customUserResolver.ts
@@ -1,25 +1,24 @@
-import { Authorized, Ctx, Field, FieldResolver, Int, Resolver, ResolverInterface, Root } from 'type-graphql'
+import { Ctx, FieldResolver, Int, Resolver, Root } from 'type-graphql'
 import { User } from '@generated/type-graphql'
 import { MyContext } from '../services/apollo'
 import { isUserAlreadyFollowed } from '../libs/user/toggleFollow'
 import { friendsCount, followersCount } from '../libs/user/followCount'
 
 @Resolver((_of) => User)
-export class customUserResolver  {
-
-  @FieldResolver(_ => Boolean)
+export class customUserResolver {
+  @FieldResolver((_) => Boolean)
   async amIFollowing(@Root() user: User, @Ctx() ctx: MyContext) {
     const uid = ctx.user?.id
-    if(!uid) return false
-    return await isUserAlreadyFollowed({uid, followUserId: user.id})
+    if (!uid) return false
+    return await isUserAlreadyFollowed({ uid, followUserId: user.id })
   }
 
-  @FieldResolver(_ => Int)
+  @FieldResolver((_) => Int)
   async followersCount(@Root() user: User) {
     return await followersCount(user.id)
   }
 
-  @FieldResolver(_ => Int)
+  @FieldResolver((_) => Int)
   async friendsCount(@Root() user: User) {
     return await friendsCount(user.id)
   }

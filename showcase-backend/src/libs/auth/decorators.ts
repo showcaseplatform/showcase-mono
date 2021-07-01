54c2c08f80dd32fe6a4b4a46f56b82da2e696264
Fidym <marci@fidy.hu>
2021-07-01 16:22:17 +0200

    fix: add missing relationship resolver with appropiate permission decorator

--- showcase-backend/src/libs/auth/decorators.ts
+++ showcase-backend/src/libs/auth/decorators.ts
@@ -2,11 +2,9 @@ import {
   NextFn,
   createParamDecorator,
   createMethodDecorator,
-  MiddlewareInterface,
-  ResolverData,
 } from 'type-graphql'
 import { MyContext } from '../../services/apollo'
-import { BadgeType, User } from '@generated/type-graphql'
+import { BadgeType, User, BadgeItem } from '@generated/type-graphql'
 
 export const CurrentUser = () => {
   return createParamDecorator<MyContext>(({ context }) => {
@@ -36,30 +34,13 @@ export const IsBadgeTypeCreatedByCurrentUser = (fallbackType? : null | []) => {
   })
 }
 
-// todo: same as IsCurrentUser but in a middleware, decide which to use in generatedResvoler.ts
-// export class isOwnedByCurrentUser implements MiddlewareInterface<MyContext> {
-//   async use({ root, context }: ResolverData<MyContext>, next: NextFn) {
-//     const isOwnedByCurrentUser = context.user?.id && context.user.id === (root as User).id
-//     console.log("isOwnedByCurrentUser")
-//     if (isOwnedByCurrentUser) {
-//       return await next()
-//     } else {
-//       return null
-//     }
-//   }
-// }
-
-// export const FallbackToCurrentUser = () => {
-//   return createParamDecorator<MyContext>(({ context }) => {
-//     console.log("FallbackToCurrentUser")
-//     return context.user
-//   })
-// }
-
-// export class FallbackToContextUser implements MiddlewareInterface<MyContext> {
-//   use({ root, context, args }: ResolverData<MyContext>, next: NextFn) {
-//     console.log({ args }, { context }, { root })
-//     // todo: implement this
-//     return next()
-//   }
-// }
+export const IsBadgeItemOwnedByCurrentUser = (fallbackType? : null | []) => {
+  return createMethodDecorator<MyContext>(async ({ context, root }, next: NextFn) => {
+    const isOwnedByCurrentUser = context.user?.id && context.user.id === (root as BadgeItem).ownerId
+    if (isOwnedByCurrentUser) {
+      return await next()
+    } else {
+      return fallbackType || null
+    }
+  })
+}

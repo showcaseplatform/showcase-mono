bcdcc7334a03247d4c6940337dc7a6686ce22ce8
Fidym <marci@fidy.hu>
2021-07-08 15:51:17 +0200

    fix: add IsBadgeItemOwnedOrCreatedByCurrentUser decorator

--- showcase-backend/src/libs/auth/decorators.ts
+++ showcase-backend/src/libs/auth/decorators.ts
@@ -4,7 +4,7 @@ import {
   createMethodDecorator,
 } from 'type-graphql'
 import { MyContext } from '../../services/apollo'
-import { BadgeType, User, BadgeItem } from '@generated/type-graphql'
+import { User, BadgeItem } from '@generated/type-graphql'
 
 export const CurrentUser = () => {
   return createParamDecorator<MyContext>(({ context }) => {
@@ -23,22 +23,11 @@ export const IsCurrentUser = (fallbackType? : null | []) => {
   })
 }
 
-// todo: not used atm, delete if cant be used anywhere
-// export const IsBadgeTypeCreatedByCurrentUser = (fallbackType? : null | []) => {
-//   return createMethodDecorator<MyContext>(async ({ context, root }, next: NextFn) => {
-//     const isCreatedByCurrentUser = context.user?.id && context.user.id === (root as BadgeType).creatorId
-//     if (isCreatedByCurrentUser) {
-//       return await next()
-//     } else {
-//       return fallbackType || null
-//     }
-//   })
-// }
-
-export const IsBadgeItemOwnedByCurrentUser = (fallbackType? : null | []) => {
+export const IsBadgeItemOwnedOrCreatedByCurrentUser = (fallbackType? : null | []) => {
   return createMethodDecorator<MyContext>(async ({ context, root }, next: NextFn) => {
     const isOwnedByCurrentUser = context.user?.id && context.user.id === (root as BadgeItem).ownerId
-    if (isOwnedByCurrentUser) {
+    const isCreatedByCurrentUser = context.user?.id && context.user.id === (root as BadgeItem)?.badgeType?.creatorId
+    if (isOwnedByCurrentUser || isCreatedByCurrentUser) {
       return await next()
     } else {
       return fallbackType || null

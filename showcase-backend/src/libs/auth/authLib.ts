c3960676c20e8b6aa424a4860d6146d005c95e23
Fidym <marci@fidy.hu>
2021-07-13 16:32:36 +0200

    chore: remove unused interface

--- showcase-backend/src/libs/auth/authLib.ts
+++ showcase-backend/src/libs/auth/authLib.ts
@@ -4,13 +4,6 @@ import prisma from '../../services/prisma'
 import { createRandomNames } from '../../utils/randoms'
 import { jwtClient } from '../../services/jsonWebToken'
 import { findUserById } from '../database/user.repo'
-import { ArgsDictionary } from 'type-graphql'
-import { MyContext } from '../../services/apollo'
-
-interface MethodDecorator {
-  args: ArgsDictionary
-  context: MyContext
-}
 
 export enum AuthError {
   isOwnUser = "Not user's profile",

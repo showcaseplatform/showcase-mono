7290f8414473ca50c98ca4906133f717acb588d6
Fidym <marci@fidy.hu>
2021-07-21 17:00:20 +0200

    fix: add requested PR changes

--- showcase-backend/src/libs/payment/addPaymentInfo.ts
+++ showcase-backend/src/libs/payment/addPaymentInfo.ts
@@ -7,28 +7,7 @@ export const addPaymentInfo = async (input: AddPaymentInfoInput, user: User): Pr
   const { idToken, lastFourCardDigit } = input || {}
 
   // todo: check user has neccesary information to connect with payment service
-  // const profile = await prisma.profile.findUnique({
-  //   where: {
-  //     id: user.id,
-  //   },
-  // })
-
-  // if (!profile || !profile.email || !user.phone) {
-  //   throw new GraphQLError('User profile is missing email or phone')
-  // }
-
   // todo: connect user with payment service provider here
-  // const stripeCustomer = await stripe.customers.create({
-  //   metadata: {
-  //     uid: user.id,
-  //     phone: user.phone,
-  //     username: profile.username,
-  //     displayName: profile.displayName,
-  //     email: profile.email,
-  //   },
-  //   source: idToken,
-  //   phone: user.phone,
-  // })
 
   const paymentInfoToSave = {
     idToken,
--- showcase-backend/src/libs/user/permissions.ts
+++ showcase-backend/src/libs/user/permissions.ts
@@ -3,7 +3,7 @@ import { SPEND_LIMIT_DEFAULT, SPEND_LIMIT_KYC_VERIFIED } from '../../consts/busi
 import { Uid } from '../../types/user'
 import { findUserWithFinancialInfo } from '../database/user.repo'
 
-export const isUserAllowedToBuy = async (id: Uid): Promise<Boolean> => {
+export const isUserAllowedToBuy = async (id: Uid): Promise<boolean> => {
   try {
     const user = await findUserWithFinancialInfo(id)
     return hasUserPaymentInfo(user)
@@ -12,26 +12,12 @@ export const isUserAllowedToBuy = async (id: Uid): Promise<Boolean> => {
   }
 }
 
-export const hasUserReachedSpendingLimit = (user: User): Boolean => {
-  if (
-    (!user.kycVerified &&
-      user.balance &&
-      user.balance.totalSpentAmountConvertedUsd > SPEND_LIMIT_DEFAULT) ||
-    (user.kycVerified &&
-      user.balance &&
-      user.balance.totalSpentAmountConvertedUsd > SPEND_LIMIT_KYC_VERIFIED)
-  ) {
-    return true
-  } else {
-    return false
-  }
+export const hasUserReachedSpendingLimit = (user: User): boolean => {
+  const spendingLimit = user.kycVerified ? SPEND_LIMIT_KYC_VERIFIED : SPEND_LIMIT_DEFAULT;
+  return (user.balance?.totalSpentAmountConvertedUsd || 0) > spendingLimit
 }
 
 // todo: improve this validation once we know what paymentInfo should we use
-export const hasUserPaymentInfo = (user: User): Boolean => {
-  if (user && user.paymentInfo?.idToken && user.paymentInfo?.lastFourCardDigit) {
-    return true
-  } else {
-    return false
-  }
+export const hasUserPaymentInfo = (user: User): boolean => {
+return !!(user.paymentInfo?.idToken && user.paymentInfo?.lastFourCardDigit)
 }

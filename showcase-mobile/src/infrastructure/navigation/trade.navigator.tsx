0b5ab5038e76a4701812574627b6e3ee10c96898
komisz <mgabor@codingsans.com>
2021-07-28 11:57:36 +0200

    chore: removed comps which should goes into next branch :smirk_cat:

--- showcase-mobile/src/infrastructure/navigation/trade.navigator.tsx
+++ showcase-mobile/src/infrastructure/navigation/trade.navigator.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import TradeBadgeDetailsScreen from '../../features/trade/screens/TradeBadgeDetails.screen'
 import TradeBadgeScreen from '../../features/trade/screens/TradeBadge.screen'
-import { MyBadgeType } from '../../features/badges/components/BadgeItem.component'
+// import TradeBadgeDetailsScreen from '../../features/trade/screens/TradeBadgeDetails.screen'
+// import { MyBadgeType } from '../../features/badges/components/BadgeItem.component'
 
 const TradeStack = createStackNavigator<TradeStackParamList>()
 
 export type TradeStackParamList = {
   TradeBadge: undefined
-  TradeBadgeDetails: {
-    item: MyBadgeType
-  }
+  // TradeBadgeDetails: {
+  //   item: MyBadgeType
+  // }
 }
 
 const TradeNavigator = () => {
@@ -25,14 +25,14 @@ const TradeNavigator = () => {
         options={{ headerShown: false }}
         component={TradeBadgeScreen}
       />
-      <TradeStack.Screen
+      {/* <TradeStack.Screen
         name="TradeBadgeDetails"
         component={TradeBadgeDetailsScreen}
         options={({ route }) => ({
           headerBackTitleVisible: false,
           headerTitle: route.params?.item.title,
         })}
-      />
+      /> */}
     </TradeStack.Navigator>
   )
 }
